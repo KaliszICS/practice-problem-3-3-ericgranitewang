33349d252e6a39c346b8969dd0d65fd25b285af9
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-04-01 13:48:23 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,42 @@
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
 
+	public static ArrayList<String> perms (String str) {
+		ArrayList<String> sus = new ArrayList<String>();
+		theFunction("", str, sus);
+		return sus;
+	}
+
+	public static void theFunction (String current, String remaining, ArrayList<String> sus) {
+		if (remaining.length() == 0) {
+			sus.add(current);
+			return;
+		}
+		for (int i = 0; i < remaining.length(); i++) {
+			theFunction(current + remaining.charAt(i), remaining.substring(0, i) + remaining.substring(i+1), sus);
+		}
+	}
+
+	public static ArrayList<String> permsUnique (String str) {
+		ArrayList<String> sus = new ArrayList<String>();
+		theFunctionUnique("", str, sus);
+		return sus;
+	}
+
+	public static void theFunctionUnique (String current, String remaining, ArrayList<String> sus) {
+		if (remaining.length() == 0) {
+			if (!sus.contains(current)) {
+				sus.add(current);
+			}
+			return;
+		}
+		for (int i = 0; i < remaining.length(); i++) {
+			theFunctionUnique(current + remaining.charAt(i), remaining.substring(0, i) + remaining.substring(i+1), sus);
+		}
+	}
 }
